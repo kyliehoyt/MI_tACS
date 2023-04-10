83fcd8c79c2098eaf829fbe35b0080b5431882b9
kyliehoyt <60266803+kyliehoyt@users.noreply.github.com>
2023-04-09 23:02:47 -0500

    changed method
    started a new method of computing the difference in alpha power during the resting state; added anova testing; started performance calculations

---
+++ alphaPower_Performance.m
@@ -0,0 +1,46 @@
+clear
+close all
+clc
+
+Subject_12 = load('Subj012.mat', 'sub').sub;
+Subject_16 = load('Subj016.mat', 'sub').sub;
+Subject_17 = load('Subj017.mat', 'sub').sub;
+fs = Subject_12.Pre.restingState.run.header.SampleRate;
+
+tRNS_Subjects = Subject_12;
+tACS_Subjects = [Subject_16, Subject_17];
+All_Subjects = [Subject_12, Subject_16, Subject_17];
+
+[all_perf,all_to] = compute_performance(All_Subjects, fs);
+
+function [accuracy, timeout_rate] = compute_performance(subjects, fs)
+    n_sub = length(subjects);
+    sessions = ["Pre", "Post"];
+    n_sess = length(sessions);
+    n_run = 3;
+    [accuracy, timeout_rate] = deal(zeros(n_sub, n_sess, n_run));
+    for sub = 1:n_sub
+        for sess = 1:n_sess
+            session = sessions(sess);
+            for run = 1:n_run
+                [n_hit, n_miss, n_to] = deal(0);
+                trigs = getfield(subjects, {sub}, session, 'Online', 'run', {run}, 'header','EVENT', 'TYP');
+                pos = getfield(subjects, {sub}, session, 'Online', 'run', {run}, 'header','EVENT', 'POS');
+                task_start = pos(find(trigs==1000)+2);
+                trial_outcome = trigs(find(trigs==1000)+3);
+                trial_end = pos(find(trigs==1000)+3);
+                for t = 1:length(trial_outcome)
+                    if trial_outcome(t) == 7693 || trial_outcome(t)==7703
+                        n_hit = n_hit+1;
+                    elseif trial_end(t)-task_start(t) < 7*fs
+                        n_miss = n_miss+1;
+                    else
+                        n_to = n_to+1;
+                    end
+                end
+                accuracy(sub, sess, run) = n_hit/(n_hit+n_miss);
+                timeout_rate(sub, sess, run) = n_to/length(trial_outcome);
+            end
+        end
+    end
+end
--- restingState_alphaPower.m
+++ restingState_alphaPower.m
@@ -24,6 +24,9 @@ tRNS_rest_psd_pvals = psd_ttest(tRNS_rest_psd, alpha, true);
 tACS_rest_psd_pvals = psd_ttest(tACS_rest_psd, alpha, false);
 All_rest_psd_pvals = psd_ttest(All_rest_psd, alpha, true);
 
+rs_alpha = buildPowerTable(All_Subjects, fs, ["tRNS", "tACS", "tACS"]);
+aov_results = rsPowerAOV(rs_alpha);
+
 function rest_psd = restruns2psd(subjects, fs, psd_band, resolution, win, winfreq)
     n_sub = length(subjects);
     sessions = ["Pre", "Post"];
@@ -64,4 +67,57 @@ function p_val = psd_ttest(psds, test_band, grouped)
             [~, p_val(sub)] = ttest(pre_psd, post_psd);
         end
     end
+end
+
+function mean_alpha_power = meanAlphaPower(segment, fs, f_band)
+    L = size(segment, 1);
+    freq_dom = fft(segment, L, 1);
+    two_sided = abs(freq_dom/L).^2;
+    single_sided = two_sided(1:L/2+1, :);
+    f = fs*(0:(L/2))/L;
+    % cut off around alpha
+    mean_alpha_power = mean(single_sided, 1);
+end
+
+function segment_means = getSegmentPowers(period, fs, seglen)
+    L = size(period, 1);
+    wSize = floor(seglen*fs); % seconds to samples
+    n_seg = floor(L/wSize);
+    segment_means = zeros(n_seg, size(period, 2));
+    for seg = 1:n_seg
+        segment = period((seg-1)*wSize+1:(seg)*wSize, :);
+        segment_means(seg, :) = meanAlphaPower(segment, fs, [8 13]);
+    end
+end
+
+function rs_alpha_power = buildPowerTable(subjects, fs, treatment_labels)
+    n_sub = length(subjects);
+    sessions = ["Pre", "Post"];
+    n_sess = length(sessions);
+    [time, treatment, segment_means] = deal([]);
+    for sub = 1:n_sub
+        for sess = 1:n_sess
+            session = sessions(sess);
+            period = getfield(subjects, {sub}, session, 'restingState', 'run', 'eeg');
+            new_means = getSegmentPowers(period, fs, 1);
+            segment_means = vertcat(segment_means, new_means);
+            treatment = vertcat(treatment, repmat([treatment_labels(sub)], size(new_means, 1), 1));
+            time = vertcat(time, repmat(session, size(new_means, 1), 1));
+        end
+    end
+    rs_alpha_power = struct('values', segment_means, 'treatment', treatment, 'time', time);
+end
+
+function aov_results = rsPowerAOV(power_struct)
+    n_chan = 32;
+    fields = {'p', 'tbl', 'stats'};
+    c = cell(length(fields), n_chan);
+    aov_results = cell2struct(c, fields);
+    for chan = 1:n_chan
+        [p, tbl, stats] = anovan(power_struct.values(:, chan), {power_struct.treatment, power_struct.time}, 'model', 2, 'varnames', {'treatment', 'time'});
+        aov_results(chan).p = p;
+        aov_results(chan).tbl = tbl;
+        aov_results(chan).stats = stats;
+    end
+    close all
 end
