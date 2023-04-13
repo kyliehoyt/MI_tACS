5b918973268c66788f1a67e0c2bf30c7e0694fcf
kyliehoyt <60266803+kyliehoyt@users.noreply.github.com>
2023-04-12 19:44:50 -0500

    changed fft to pwelch
    changed initial filter to [1 50] and changed fft implementation to pwelch

--- alphaPower_Performance.m
+++ alphaPower_Performance.m
@@ -11,22 +11,20 @@ fs = Subject_12.Pre.restingState.run.header.SampleRate;
 tRNS_Subjects = Subject_12;
 tACS_Subjects = [Subject_16, Subject_17];
 All_Subjects = [Subject_12, Subject_16, Subject_17];
-All_Subjects = cleanSubjects(All_Subjects, fs, 4, [4 30]);
+All_Subjects = cleanSubjects(All_Subjects, fs, 4, [1 50]);
 
 %% Compute Performance Metrics
 [all_perf,all_to] = compute_performance(All_Subjects, fs);
 
 %% Compute Absolute Alpha Power
-ffts = getSubjectFFTs(All_Subjects, fs);
-GA_ffts = calcGAvgFFT(ffts);
-test_fft = GA_ffts(1).Pre(:, 15);
-n = fs/2;
-f = (fs*(0:(n/2))/n)';
+[ffts, f] = getSubjectFFTs(All_Subjects, fs);
+test_fft = ffts(3).Pre(:, 15);
 
-D = fittype('A/(1-abs(4*(x-c)/w))*0.5*(1-cos(2*pi()*x/w))+N*x.^-B','dependent', {'D'}, 'independent', {'x'}, 'coefficients', {'N', 'B', 'A', 'c', 'w'});
-fo = fitoptions('StartPoint', [0, 0.45, 0.5, 10, 5], 'Method', 'NonlinearLeastSquares', 'Lower', [0, 0.2, 0, 8, 2], 'Upper', [1, 0.8, 1, 12, 6]);
-% fitobject = fit(f, test_fft, D, fo);
-% plot(fitobject, f, test_fft)
+
+D = fittype('a+b/f.^c','dependent', {'D'}, 'independent', {'f'}, 'coefficients', {'a', 'b', 'c'});
+fo = fitoptions('StartPoint', [0, 0, 0], 'Method', 'NonlinearLeastSquares', 'Lower', [0, 0, 0], 'Upper', [5, 5, 5]);
+fitobject = fit(f, test_fft, D, fo);
+plot(fitobject, f, 10*log(test_fft))
 plotFFT(f, test_fft)
 
 
@@ -63,70 +61,31 @@ function [accuracy, timeout_rate] = compute_performance(subjects, fs)
     end
 end
 
-function [single_sided, f] = calcSegmentFFT(segment, fs, Display)
-    L = size(segment, 1);
-    n = 2^nextpow2(L);
-    %n = fs*2;
-    n = L/2;
-    freq_dom = fft(segment, n, 1);
-    two_sided = abs(freq_dom/n).^2;
-    single_sided = two_sided(2:(n/2)+2, :);
-    f = (fs*(0:(n/2))/n)';
-    if Display
-        plotFFT(f, single_sided);
-    end
-end
-
-function plotFFT(f, single_sided)
-    plot(f,single_sided) 
+function plotFFT(f, fft)
+    plot(f,10*log(fft)) 
     title("Single-Sided Power Spectrum")
     xlabel("f (Hz)")
-    ylabel("|P(f)|^2")
+    ylabel("Power (dB)")
     xlim([0 50])
 end
 
-function segment_FFT = getSegmentFFTs(period, fs, seglen)
-    L = size(period, 1);
-    wSize = floor(seglen*fs); % seconds to samples
-    n_seg = floor(L/wSize);
-    segment_FFT = zeros(n_seg, fs/4+1, size(period, 2));
-    for seg = 1:n_seg
-        segment = period((seg-1)*wSize+1:(seg)*wSize, :);
-        [segment_FFT(seg, :, :), ~] = calcSegmentFFT(segment, fs, false);
-    end
-end
 
-function subject_FFT = getSubjectFFTs(subjects, fs)
+function [subject_FFT, f] = getSubjectFFTs(subjects, fs)
     n_sub = length(subjects);
     sessions = {'Pre', 'Post'};
     n_sess = length(sessions);
-    n_chan = 32;
     c = cell(n_sess, n_sub);
     subject_FFT = cell2struct(c, sessions);
     for sub = 1:n_sub
         for sess = 1:n_sess
             session = sessions{sess};
             period = getfield(subjects, {sub}, session, 'restingState', 'run', 'eeg');
-            period = trimRun(period, n_chan);
-            subject_FFT = setfield(subject_FFT, {sub}, session, getSegmentFFTs(period, fs, 1)); % 1 second segments
+            [pxx, f] = pwelch(period(10*fs:end-10*fs,:), fs, fs/2, 1:50, fs);
+            subject_FFT = setfield(subject_FFT, {sub}, session, pxx); % 1 second segments
         end
     end
 end
 
-function grand_average_fft = calcGAvgFFT(subject_FFT)
-    n_sub = length(subject_FFT);
-    sessions = {'Pre', 'Post'};
-    n_sess = length(sessions);
-    c = cell(n_sess, n_sub);
-    grand_average_fft = cell2struct(c, sessions);
-    for sub = 1:n_sub
-        for sess = 1:n_sess
-            session = sessions{sess};
-            session_fft = getfield(subject_FFT, {sub}, session);
-            grand_average_fft = setfield(grand_average_fft, {sub}, session, squeeze(mean(session_fft, 1)));
-        end
-    end
-end
 
 % function to see if there is a statistically significant difference in CDA and TO before and after
 % tACS
